acaf052eb687545cfd1d7f61786e930302969540
luoshiqing <644402920@qq.com>
2017-01-12 09:37:14 +0800

    Initial Commit

---
+++ SUV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SUV
+//
+//  Created by sqluo on 2017/1/12.
+//  Copyright © 2017年 sqluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SUV/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SUV
+//
+//  Created by sqluo on 2017/1/12.
+//  Copyright © 2017年 sqluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
